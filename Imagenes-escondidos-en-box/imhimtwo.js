1c364ab04661fe9404496ae0e0a33e0263911475
lexzer42 <134535937+lexzer42@users.noreply.github.com>
2023-06-26 21:38:22 -0400

    Add files via upload

---
+++ Imagenes-escondidos-en-box/imhimtwo.js
@@ -0,0 +1,31 @@
+
+const gallery = document.getElementById("gallery");
+if (gallery) {
+      
+      window.onmousemove = e => {
+window.onmousemove = e => {
+  const mouseX = e.clientX,
+        mouseY = e.clientY;
+  
+  const xDecimal = mouseX / window.innerWidth,
+        yDecimal = mouseY / window.innerHeight;
+  
+  const maxX = gallery.offsetWidth - window.innerWidth,
+        maxY = gallery.offsetHeight - window.innerHeight;
+  
+  const panX = maxX * xDecimal * -1,
+        panY = maxY * yDecimal * -1;
+  
+  gallery.animate({
+    transform: `translate(${panX}px, ${panY}px)`
+  }, {
+    duration: 4000,
+    fill: "forwards",
+    easing: "ease"
+  })
+}
+
+}
+} else {
+      console.error("No se pudo encontrar un elemento con el ID 'gallery'");
+}
